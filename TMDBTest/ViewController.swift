b33a9b334d4be992e3701d4296d4c1546ad8b3b8
Artur <arturremizov@gmail.com>
2022-03-28 15:37:36 +0300

    Initial Commit

---
+++ TMDBTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TMDBTest
+//
+//  Created by Artur Remizov on 28.03.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
